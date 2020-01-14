bcf59be495192b0e551111047c4c66a2f01a1d86
jza201 <jza201@sfu.ca>
2020-01-14 10:14:26 -0800

    fix syntax error

--- com_radiam/migrations/Migration20191125000001ComRadiam.php
+++ com_radiam/migrations/Migration20191125000001ComRadiam.php
@@ -82,7 +82,7 @@ class Migration20191125000001ComRadiam extends Base
 			`created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 			`last_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 			PRIMARY KEY (`id`),
-			KEY `idx_project` (`project_id`),
+			KEY `idx_project` (`project_id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
 			$this->db->setQuery($query);
